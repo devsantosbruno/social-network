cb731db66eb9dd8fcf46a2660a795764a01a8f7d
bruno santos <devbrunosantos@gmail.com>
2023-11-11 13:05:07 -0300

    chore: removing styles from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import Home from '@screens/Home';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 
 export default function App() {
   return (
@@ -10,12 +9,3 @@ export default function App() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#242424',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
