de3106e3667fba6272c4ce01b3a447dff1778e18
bruno santos <devbrunosantos@gmail.com>
2023-11-11 23:58:52 -0300

    feat: adding new value to SPACINGS from METRICS

--- src/themes/METRICS.ts
+++ src/themes/METRICS.ts
@@ -30,10 +30,12 @@ export const METRICS = {
   },
 
   SPACING: {
-    XS: 4,
-    SM: 8,
+    XXS: 4,
+    XS: 8,
+    SM: 12,
     DEFAULT: 16,
     LG: 24,
     XL: 32,
+    XXL: 48,
   },
-};
+} as const;
