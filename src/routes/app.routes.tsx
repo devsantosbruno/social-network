fd4d934c60e81628cc1098f7401c9b77983cad25
bruno santos <devbrunosantos@gmail.com>
2023-11-12 16:48:07 -0300

    feat: route for profile page

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,20 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '@screens/Home';
 import Login from '@screens/Login';
+import Profile from '@screens/Profile';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }} initialRouteName='login'>
-      <Screen name='login' component={Login} />
-      <Screen name='home' component={Home} />
+    <Navigator initialRouteName='login'>
+      <Screen name='login' component={Login} options={{ headerShown: false }} />
+      <Screen name='home' component={Home} options={{ headerShown: false }} />
+      <Screen
+        name='profile'
+        component={Profile}
+        options={{ headerShown: false }}
+      />
     </Navigator>
   );
 }
