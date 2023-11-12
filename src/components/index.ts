155902a5c5a7cb3d07f6c27f52272fdd457f9432
bruno santos <devbrunosantos@gmail.com>
2023-11-12 16:11:02 -0300

    chore: exporting components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { Divider } from './Divider';
+export { Image } from './Image';
+export { Interaction } from './Interaction';
+export { Loading } from './Loading';
+export { Post } from './Post';
+export { Typography } from './Typography';
