adf8473ce260dae79b2a00fb9170c8740b4e74a5
bruno santos <devbrunosantos@gmail.com>
2023-11-12 16:46:45 -0300

    feat: mockData

---
+++ src/utils/mockData.ts
@@ -0,0 +1,66 @@
+import userImage from '@assets/background-02.png';
+
+export const user = {
+  image: userImage,
+  user: '@devbrunosantos',
+  name: 'Bruno Santos',
+  job: 'Software Developer',
+  link: {
+    title: 'Portfolio',
+    url: 'https://devbruno.me/',
+  },
+  statistics: {
+    followers: 1080,
+    posts: 10,
+  },
+  posts: [
+    {
+      id: 0,
+      user: {
+        image: userImage,
+        user: '@devbrunosantos',
+        name: 'Bruno Santos',
+      },
+      title: 'BLBABLABLBLALBALBA',
+      description: 'lorem DSUHIDSAIHUDHAIS dusiAHUDISAHIUD',
+      date: '12 de outubro de 2023',
+      engagements: {
+        likes: 123,
+        comments: 45,
+        shares: 6,
+      },
+    },
+    {
+      id: 1,
+      user: {
+        image: userImage,
+        user: '@devbrunosantos',
+        name: 'Bruno Santos',
+      },
+      title: 'BLBABLABLBLALBALBA',
+      description: 'lorem DSUHIDSAIHUDHAIS dusiAHUDISAHIUD',
+      date: '12 de outubro de 2023',
+      engagements: {
+        likes: 123,
+        comments: 45,
+        shares: 6,
+      },
+    },
+    {
+      id: 2,
+      user: {
+        image: userImage,
+        user: '@devbrunosantos',
+        name: 'Bruno Santos',
+      },
+      title: 'BLBABLABLBLALBALBA',
+      description: 'lorem DSUHIDSAIHUDHAIS dusiAHUDISAHIUD',
+      date: '12 de outubro de 2023',
+      engagements: {
+        likes: 123,
+        comments: 45,
+        shares: 6,
+      },
+    },
+  ],
+};
