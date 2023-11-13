22d5509cb531b50d001dee009ebba7ed750eb9fd
bruno santos <devbrunosantos@gmail.com>
2023-11-12 21:35:39 -0300

    refactor: adding circle variant

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,14 +1,22 @@
+import { METRICS } from '@themes';
 import { Image as ImageRN, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
 type ImageProps = {
   source: ImageData;
   size: number;
-  borderSize: number;
+  borderSize?: number;
+  circle?: boolean;
 };
 
-export function Image({ source, size, borderSize }: ImageProps) {
-  const customStyles = { width: size, height: size, borderWidth: borderSize };
+export function Image({ source, size, borderSize, circle }: ImageProps) {
+  const radiusStyle = circle ? METRICS.RADIUS.PILL : METRICS.RADIUS.DEFAULT;
+  const customStyles = {
+    width: size,
+    height: size,
+    borderWidth: borderSize,
+    borderRadius: radiusStyle,
+  };
 
   return (
     <TouchableOpacity style={[styles.container, customStyles]}>
--- src/components/Image/styles.ts
+++ src/components/Image/styles.ts
@@ -1,17 +1,14 @@
-import { COLORS, METRICS } from '@themes';
+import { COLORS } from '@themes';
 import { StyleSheet } from 'react-native';
-
-const RADIUS = METRICS.RADIUS.DEFAULT;
 
 export const styles = StyleSheet.create({
   container: {
     borderColor: COLORS.NEUTRAL.WHITE,
-    borderRadius: RADIUS,
+    overflow: 'hidden',
   },
   image: {
     flex: 1,
     width: '100%',
     height: '100%',
-    borderRadius: RADIUS,
   },
 });
