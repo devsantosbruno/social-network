86e42f0b2d86f4b00ac1c68bae2f0b5cbdaa5cd7
bruno santos <devbrunosantos@gmail.com>
2023-11-12 21:36:10 -0300

    refactor: change posts and adding friends in mock data

--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -18,11 +18,12 @@ export const user = {
       id: 0,
       user: {
         image: userImage,
-        user: '@devbrunosantos',
-        name: 'Bruno Santos',
+        user: '@by_trama',
+        name: 'Trama',
       },
-      title: 'BLBABLABLBLALBALBA',
-      description: 'lorem DSUHIDSAIHUDHAIS dusiAHUDISAHIUD',
+      title: 'Do you wanna freela?',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum a sapiente rerum obcaecati sint repudiandae reprehenderit quod assumenda voluptatem veniam odit necessitatibus debitis, error saepe! Accusamus aperiam rem iure? A.',
       date: '12 de outubro de 2023',
       engagements: {
         likes: 123,
@@ -37,30 +38,40 @@ export const user = {
         user: '@devbrunosantos',
         name: 'Bruno Santos',
       },
-      title: 'BLBABLABLBLALBALBA',
-      description: 'lorem DSUHIDSAIHUDHAIS dusiAHUDISAHIUD',
+      title: 'When things go wrong, how hard do you try?',
+      description: `How much effort do you put into something that's correct but not perfect? I've always tried after countless failures in search of perfection. Is it wrong? Someday it's gonna work. Is that right? Bet it's not perfect! Am I satisfied with my tenth version of my portfolio? No, and I'm sure I'll make endless changes and even start over.`,
       date: '12 de outubro de 2023',
       engagements: {
-        likes: 123,
-        comments: 45,
-        shares: 6,
+        likes: 78,
+        comments: 9,
+        shares: 2,
       },
     },
     {
       id: 2,
       user: {
         image: userImage,
-        user: '@devbrunosantos',
-        name: 'Bruno Santos',
+        user: '@by_trama',
+        name: 'Trama',
       },
-      title: 'BLBABLABLBLALBALBA',
-      description: 'lorem DSUHIDSAIHUDHAIS dusiAHUDISAHIUD',
+      title: 'Do you wanna freela?',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum a sapiente rerum obcaecati sint repudiandae reprehenderit quod assumenda voluptatem veniam odit necessitatibus debitis, error saepe! Accusamus aperiam rem iure? A.',
       date: '12 de outubro de 2023',
       engagements: {
-        likes: 123,
-        comments: 45,
-        shares: 6,
+        likes: 217,
+        comments: 49,
+        shares: 22,
       },
     },
   ],
 };
+
+export const friends = [
+  { image: userImage, onPress: null },
+  { image: userImage, onPress: null },
+  { image: userImage, onPress: null },
+  { image: userImage, onPress: null },
+  { image: userImage, onPress: null },
+  { image: userImage, onPress: null },
+];
