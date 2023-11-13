54fa31209796e66a615341d621beef5fffbcbaf5
bruno santos <devbrunosantos@gmail.com>
2023-11-12 21:37:11 -0300

    feat: Home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,89 @@
-import { Text, View } from 'react-native';
+import { FlatList, ScrollView, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
+import userImage from '@assets/background-02.png';
+import { Image, InputGroup, Publication, Typography } from '@components';
+import { friends, user } from 'src/utils/mockData';
+
+import { COLORS, METRICS } from '@themes';
+import { PlusIcon, SearchIcon } from 'lucide-react-native';
 import { styles } from './styles';
 
 export default function Home() {
+  const { name, image } = user;
+
+  const handleCreateNewStory = () => {
+    console.log('CREATE NEW STORY');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>HOME</Text>
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <ScrollView>
+        <View style={[styles.container, styles.head]}>
+          <View style={styles.name}>
+            <Typography variant='bodyThin' style={styles.textWhite}>
+              Hi,
+            </Typography>
+            <Typography variant='heading02Bold' style={styles.textWhite}>
+              {name}!
+            </Typography>
+          </View>
+
+          <Image size={80} source={image} borderSize={1} circle />
+        </View>
+
+        <View style={styles.container}>
+          <InputGroup
+            placeholder='Search friend'
+            icon={
+              <SearchIcon
+                color={COLORS.NEUTRAL.WHITE}
+                size={METRICS.ICON.SMALL}
+              />
+            }
+          />
+        </View>
+
+        <View style={styles.stories}>
+          <FlatList
+            data={friends}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            ListHeaderComponent={
+              <TouchableOpacity
+                style={styles.createStory}
+                onPress={handleCreateNewStory}
+              >
+                <PlusIcon color={COLORS.SECONDARY} size={METRICS.ICON.MEDIUM} />
+              </TouchableOpacity>
+            }
+            renderItem={() => (
+              <View style={styles.story}>
+                <Image size={80} source={userImage} borderSize={1} circle />
+              </View>
+            )}
+          />
+        </View>
+
+        <View style={styles.container}>
+          <Typography variant='heading' style={styles.textWhite}>
+            Posts
+          </Typography>
+
+          <View style={styles.posts}>
+            {user.posts.map((item) => (
+              <Publication
+                key={item.id}
+                user={item.user}
+                title={item.title}
+                description={item.description}
+                date={item.date}
+                engagements={item.engagements}
+              />
+            ))}
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,16 +1,50 @@
-import { COLORS } from '@themes';
+import { COLORS, METRICS } from '@themes';
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
     backgroundColor: COLORS.BACKGROUND.DARK,
-    padding: 24,
-    paddingTop: 50,
   },
-  title: {
+  container: {
+    paddingHorizontal: METRICS.SPACING.LG,
+  },
+
+  head: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: METRICS.SPACING.XL,
+  },
+  name: {
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    gap: METRICS.SPACING.XS,
+  },
+
+  stories: {
+    flexDirection: 'row',
+    marginVertical: METRICS.SPACING.XL,
+  },
+  story: {
+    paddingRight: METRICS.SPACING.DEFAULT,
+  },
+  createStory: {
+    flex: 1,
+    width: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: METRICS.SPACING.DEFAULT,
+    borderRadius: METRICS.RADIUS.PILL,
+    backgroundColor: COLORS.NEUTRAL.WHITE,
+  },
+
+  posts: {
+    gap: METRICS.SPACING.XL,
+    marginTop: METRICS.SPACING.DEFAULT,
+  },
+
+  textWhite: {
     color: COLORS.NEUTRAL.WHITE,
-    fontSize: 24,
-    fontWeight: '700',
   },
 });
